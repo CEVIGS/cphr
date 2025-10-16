503558346f9a65907300a912a3169a9a37c156e0
bpph <mfeise_c21@kegs.org.uk>
2025-10-16 19:57:43 +0100

    feat: caesar custom mapper

--- cphr/caesar.py
+++ cphr/caesar.py
@@ -1,10 +1,14 @@
 import string
+from typing import Callable, Optional
 
 
-def caesar(text: str, shift: int, alphabet: str = string.ascii_uppercase) -> str:
+def caesar(text: str, shift: int, alphabet: str = string.ascii_uppercase, mapper: Optional[Callable[[int], int]] = None) -> str:
+    if mapper is None:
+        mapper = lambda n: n + shift
+
     def mapping(char: str) -> str:
         if char in alphabet:
-            return alphabet[(alphabet.index(char) + shift) % len(alphabet)]
+            return alphabet[mapper(alphabet.index(char)) % len(alphabet)]
         return char
 
     return ''.join(map(mapping, text))
